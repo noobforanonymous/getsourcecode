af0d93b39ad8202404dc070008a17090fddb02ad
anonymous <78273014+noobforanonymous@users.noreply.github.com>
2021-02-06 23:37:45 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,39 +6,42 @@ clear
 echo " "
 echo " "
 echo '
-               
-               
-figlet regaan '|lolcat
+    _____     _____)  _____)  _____    _____    __     __) 
+  (, /   ) /       /        (, /  |  (, /  |  (, /|  /    
+    /__ /  )__    /   ___     /---|    /---|    / | /     
+ ) /   \_/       /     / ) ) /    |_) /    |_) /  |/      
+(_/     (_____) (____ /   (_/      (_/      (_/   '      
+
+'|lolcat
 echo " "
 echo " "
 sleep 6.0
 clear
-echo -e "$red                         wait..>$clear"
+echo -e "$red                         <===================wait===============>$clear"
 sleep 2.0
 clear
-echo -e "$red                         we are checking...>$clear"
+echo -e "$red                          <===================we are checking===============>$clear"
 sleep 2.0
 clear
-echo -e "$red                         any updates....>$clear"
+echo -e "$red                         <===================any updates===============>$clear"
 sleep 2.0
 clear
-echo -e "$red                         wait now it's .....>$clear"
+echo -e "$red                         <===================wait now it's===============>$clear"
 sleep 2.0
 clear
 echo " "
 echo " "
-echo -e "$green                updating the getsourcecode please wait$clear"
+echo -e "$green               <=================== updating the getsourcecode please wait===============>$clear"
 sleep 2.0
 cd $HOME
 rm -rf getsourcecode
 git clone https://github.com/noobforanonymous/getsourcecode
 clear
 echo " "
-echo -e "$yellow               getsourcecode has been updated check it now...$clear"
+echo -e "$yellow              <=================== getsourcecode has been updated check it now===============>$clear"
 sleep 3.0
 echo " "
 cd $HOME
 cd getsourcecode
-python3 sourcecode.py
-clear
-sleep 1.0
+
+
